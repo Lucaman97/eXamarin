d08f6ad2f50f5207155b5786147eac037c55f08d
Lucaman97 <s1080190@studenti.univpm.it>
2019-08-16 12:46:51 +0200

    ora funzionante*

---
+++ eXamarin/eXamarin/eXamarin/LoginPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace eXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class LoginPage : ContentPage
+    {
+
+        Button loginButton;
+        Button registerButton;
+        StackLayout stackLayout;
+        public LoginPage()
+        {
+            InitializeComponent();
+            userNameEntry = new Entry
+            {
+                Placeholder = "username"
+            };
+            passwordEntry = new Entry
+            {
+                Placeholder = "password",
+                IsPassword = true
+            };
+            loginButton = new Button
+            {
+                Text = "Login"
+            };
+            loginButton.Clicked += LoginButton_Clicked;
+            this.Padding = new Thickness(20);
+            stackLayout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Orientation = StackOrientation.Vertical,
+                Spacing = 10,
+                Children = {
+                        userNameEntry,
+                        passwordEntry,
+                        loginButton
+                    }
+            };
+
+            this.Content = stackLayout;
+        }
+        void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            string URL = "http://mobileproject.altervista.org/login.php";
+            MakeRequest.setPost(userNameEntry.Text, passwordEntry.Text, URL);
+        }
+    }
+}
---
+++ eXamarin/eXamarin/eXamarin/MakeRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+
+namespace eXamarin
+{
+    class MakeRequest
+    {
+        private static HttpClient _client = new HttpClient();
+        public static async void setPost(String username, String pass, String URL)
+        {
+            HttpContent formcontent = new FormUrlEncodedContent(new[] {
+                new KeyValuePair<string,string>("login_name", username),
+                new KeyValuePair<string, string>("login_pass", pass)
+            });
+
+            var response = await _client.PostAsync(URL, formcontent);
+            var result = response.Content.ReadAsStringAsync().Result.ToString();
+            Debug.WriteLine(result);
+            if (result.Equals("    Login Success  "))
+            {
+                Debug.WriteLine("Funge");
+            }
+
+            else
+            {
+                Debug.WriteLine("Error while inserting User in Post mode");
+            }
+
+        }
+
+
+    }
+}
